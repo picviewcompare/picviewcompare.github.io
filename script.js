7f3ca82145816fcee9fc896171d23774578b198f
username-matthew <matthew@olk1.com>
2023-07-24 22:18:35 +0100

    reformat code for better readability/organisation

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ function handleImageUpload(event) {
     const imageURL = event.target.result;
     imageElement.src = imageURL;
 
-    // Save the image data to localStorage
+// Save the image data to localStorage
     saveImageData(imageIndex, imageURL);
-    // Add a class to the label element to indicate that an image has been uploaded
+
+// Add a class to the label element to indicate that an image has been uploaded
     labelElement.classList.add('image-has-been-uploaded');
   }
 
@@ -24,44 +25,15 @@ function handleImageUpload(event) {
   }
 }
 
-function saveImageData(imageIndex, imageURL) {
-  const imageArray = getSavedImageArray();
-  imageArray[imageIndex] = imageURL;
-  localStorage.setItem('uploaded_images', JSON.stringify(imageArray));
-}
-
-function loadImagesFromLocalStorage() {
-  const imageArray = getSavedImageArray();
-
-  for (let i = 0; i < imageGrid.children.length; i++) {
-    const gridItem = imageGrid.children[i];
-    const imageElement = gridItem.querySelector('.uploaded-image');
-    const labelElement = gridItem.querySelector('.upload-label');
-    const imageIndex = parseInt(imageElement.getAttribute('data-index'), 10);
-    const imageURL = imageArray[imageIndex];
-
-    if (imageURL) {
-      imageElement.src = imageURL;
-      labelElement.classList.add('image-has-been-uploaded'); // Add the class if an image is present
-    }
-  }
-}
-
-function getSavedImageArray() {
-  const savedImageData = localStorage.getItem('uploaded_images');
-  return savedImageData ? JSON.parse(savedImageData) : [];
-}
-
-loadImagesFromLocalStorage();
 
 function toggleFullScreen(imageElement) {
   const gridItem = imageElement.closest('.grid-item');
   if (!gridItem.classList.contains('full-screen')) {
-    // Expand the image to full screen
+// Expand the image to full screen
     gridItem.classList.add('full-screen');
     document.addEventListener('keydown', handleEscapeKey);
   } else {
-    // Return the image to the original position in the grid
+// Return the image to the original position in the grid
     gridItem.classList.remove('full-screen');
     document.removeEventListener('keydown', handleEscapeKey);
   }
@@ -92,6 +64,37 @@ function generateUniqueInputIDs() {
 document.addEventListener('DOMContentLoaded', generateUniqueInputIDs);
 
 
+// 
+// LOCALSTORAGE
+function saveImageData(imageIndex, imageURL) {
+  const imageArray = getSavedImageArray();
+  imageArray[imageIndex] = imageURL;
+  localStorage.setItem('uploaded_images', JSON.stringify(imageArray));
+}
+
+function loadImagesFromLocalStorage() {
+  const imageArray = getSavedImageArray();
+
+  for (let i = 0; i < imageGrid.children.length; i++) {
+    const gridItem = imageGrid.children[i];
+    const imageElement = gridItem.querySelector('.uploaded-image');
+    const labelElement = gridItem.querySelector('.upload-label');
+    const imageIndex = parseInt(imageElement.getAttribute('data-index'), 10);
+    const imageURL = imageArray[imageIndex];
+
+    if (imageURL) {
+      imageElement.src = imageURL;
+      labelElement.classList.add('image-has-been-uploaded'); // Add the class if an image is present
+    }
+  }
+}
+
+function getSavedImageArray() {
+  const savedImageData = localStorage.getItem('uploaded_images');
+  return savedImageData ? JSON.parse(savedImageData) : [];
+}
+
+loadImagesFromLocalStorage();
 
 // Get a reference to the clear button
 const clearButton = document.getElementById('clearLocalStorageButton');
