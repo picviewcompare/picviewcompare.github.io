e5b73ef100264e8c1acb2d66ca6b6c0ebe615b54
username-matthew <matthew@olk1.com>
2023-07-25 11:29:53 +0100

    left and right arrow move between images in fullscreen infinitely

--- script.js
+++ script.js
@@ -23,26 +23,54 @@ function handleImageUpload(event) {
   }
 }
 
-
 function toggleFullScreen(imageElement) {
   const gridItem = imageElement.closest('.grid-item');
   if (!gridItem.classList.contains('full-screen')) {
-
     gridItem.classList.add('full-screen');
-    document.addEventListener('keydown', handleEscapeKey);
+    document.addEventListener('keydown', handleKeyPress);
   } else {
-
     gridItem.classList.remove('full-screen');
-    document.removeEventListener('keydown', handleEscapeKey);
+    document.removeEventListener('keydown', handleKeyPress);
   }
 }
 
-function handleEscapeKey(event) {
+function handleKeyPress(event) {
   if (event.key === 'Escape') {
     const fullscreenImage = document.querySelector('.full-screen');
     if (fullscreenImage) {
       fullscreenImage.classList.remove('full-screen');
-      document.removeEventListener('keydown', handleEscapeKey);
+      document.removeEventListener('keydown', handleKeyPress);
+    }
+  } else if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+    const fullscreenImage = document.querySelector('.full-screen .uploaded-image');
+    if (fullscreenImage) {
+      const imageElements = document.querySelectorAll('.uploaded-image');
+      const fullscreenImageIndex = parseInt(fullscreenImage.getAttribute('data-index'), 10);
+      const lastIndex = imageElements.length - 1;
+      let newIndex;
+
+      if (event.key === 'ArrowLeft') {
+        newIndex = fullscreenImageIndex === 0 ? lastIndex : fullscreenImageIndex - 1;
+      } else {
+        newIndex = fullscreenImageIndex === lastIndex ? 0 : fullscreenImageIndex + 1;
+      }
+
+      // Find the next valid img element by skipping empty grid-items
+      let nextImage = null;
+      for (let i = 0; i < imageElements.length; i++) {
+        const imgIndex = parseInt(imageElements[i].getAttribute('data-index'), 10);
+        if (!isNaN(imgIndex) && imgIndex === newIndex) {
+          nextImage = imageElements[i];
+          break;
+        }
+      }
+
+      if (nextImage) {
+        const fullscreenGridItem = fullscreenImage.closest('.grid-item');
+        fullscreenGridItem.classList.remove('full-screen');
+        document.removeEventListener('keydown', handleKeyPress);
+        toggleFullScreen(nextImage);
+      }
     }
   }
 }
@@ -59,6 +87,22 @@ function generateUniqueInputIDs() {
 }
 
 document.addEventListener('DOMContentLoaded', generateUniqueInputIDs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 
